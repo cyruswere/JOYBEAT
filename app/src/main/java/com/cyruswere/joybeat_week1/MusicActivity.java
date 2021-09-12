a6b7fc28e832f2a8fbc2373dd4e8d8be02edfb95
cyruswere <cyrus.were@student.moringschool.com>
2021-09-12 18:59:24 +0300

    initial commit

---
+++ app/src/main/java/com/cyruswere/joybeat_week1/MusicActivity.java
@@ -0,0 +1,14 @@
+  package com.cyruswere.joybeat_week1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+}
